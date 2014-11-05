4c4a0e2fc2ab7e952499946560f65383aac008d8
deansher <deansherthompson@gmail.com>
2014-11-05 13:17:59 -0500

    Adding new files generated by lein. (Left them out last time.)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to codemining-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject codemining-clojure "0.1.0-SNAPSHOT"
+  :description "an exploratory implementation of Code Mining in and for Clojure"
+  :url "https://github.com/WorldWideHack/codemining-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot codemining-clojure.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
